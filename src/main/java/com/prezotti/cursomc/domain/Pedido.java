186d2ec506bbd59ba99b4e8a196ebbce6fb02188
Prezotti <henriqueprezotti@gmail.com>
2022-04-13 14:18:32 -0300

    Atualizacao: Utilizacao apenas do JsonIgnore

--- src/main/java/com/prezotti/cursomc/domain/Pedido.java
+++ src/main/java/com/prezotti/cursomc/domain/Pedido.java
@@ -17,7 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable{
@@ -29,12 +28,10 @@ public class Pedido implements Serializable{
 	
 	@JsonFormat(pattern="dd/MM/yyy HH:mm")
 	private Date instante;
-	
-	@JsonManagedReference
+
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
-	
-	@JsonManagedReference
+
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
