1f5a11047b54bbd42a137a28c6f68af38a505171
Prezotti <henriqueprezotti@gmail.com>
2022-04-13 09:51:06 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/prezotti/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/prezotti/cursomc/resources/CategoriaResource.java
@@ -20,9 +20,7 @@ public class CategoriaResource {
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
-		return ResponseEntity.ok().body(obj);
-		
+		return ResponseEntity.ok().body(obj);	
 
 	}
-
 }
--- src/main/java/com/prezotti/cursomc/services/CategoriaService.java
+++ src/main/java/com/prezotti/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.prezotti.cursomc.domain.Categoria;
 import com.prezotti.cursomc.repositories.CategoriaRepository;
+import com.prezotti.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,8 +16,9 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElse(null); 
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 	}
 
 }
