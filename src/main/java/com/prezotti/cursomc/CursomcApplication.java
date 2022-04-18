afe8e5d2d092b269ff0e5b74103ac1e87a403b39
Prezotti <henriqueprezotti@gmail.com>
2022-04-18 18:58:40 -0300

    Configuracao e envio do projeto para producao

--- src/main/java/com/prezotti/cursomc/CursomcApplication.java
+++ src/main/java/com/prezotti/cursomc/CursomcApplication.java
@@ -12,10 +12,7 @@ public class CursomcApplication implements CommandLineRunner{
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		
-		
-		
+	public void run(String... args) throws Exception {		
 	}	
 
 }
