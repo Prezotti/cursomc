eb7089b977eba9df5234281c4020084269a51bc9
Prezotti <henriqueprezotti@gmail.com>
2022-04-18 10:54:36 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/prezotti/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/prezotti/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.prezotti.cursomc.domain.Cliente;
+import com.prezotti.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
