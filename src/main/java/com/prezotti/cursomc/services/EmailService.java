fd465f5b7a1ab8a70436e71615e16b649d1eefdf
Prezotti <henriqueprezotti@gmail.com>
2022-04-19 10:28:00 -0300

    Email HTML

--- src/main/java/com/prezotti/cursomc/services/EmailService.java
+++ src/main/java/com/prezotti/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.prezotti.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.prezotti.cursomc.domain.Pedido;
@@ -9,4 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+	
 }
