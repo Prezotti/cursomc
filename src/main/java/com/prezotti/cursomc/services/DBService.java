302f047082795b7d2e75ad7f0c1d07a3ae7fef96
Prezotti <henriqueprezotti@gmail.com>
2022-04-21 20:37:13 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/prezotti/cursomc/services/DBService.java
+++ src/main/java/com/prezotti/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.prezotti.cursomc.domain.PagamentoComCartao;
 import com.prezotti.cursomc.domain.Pedido;
 import com.prezotti.cursomc.domain.Produto;
 import com.prezotti.cursomc.domain.enums.EstadoPagamento;
+import com.prezotti.cursomc.domain.enums.Perfil;
 import com.prezotti.cursomc.domain.enums.TipoCliente;
 import com.prezotti.cursomc.repositories.CategoriaRepository;
 import com.prezotti.cursomc.repositories.CidadeRepository;
@@ -114,16 +115,21 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "henriquegp45@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli2 = new Cliente(null, "Ana Costa", "henriqueprezotti@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
