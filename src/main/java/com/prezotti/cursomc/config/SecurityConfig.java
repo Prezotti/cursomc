9f2eb397275088e715b2e65f0c740c9e3c4b5496
Prezotti <henriqueprezotti@gmail.com>
2022-04-26 15:28:33 -0300

    Implementando Autorizacao

--- src/main/java/com/prezotti/cursomc/config/SecurityConfig.java
+++ src/main/java/com/prezotti/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.prezotti.cursomc.security.JWTAuthenticationFilter;
+import com.prezotti.cursomc.security.JWTAuthorizationFilter;
 import com.prezotti.cursomc.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
